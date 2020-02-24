aa06837440869e259a85ecf9ca2b1e7a6a97f821
Jitendra Kumar <jitudsce52@gmail.com>
2020-02-24 17:22:27 +0900

    modify volume path

--- Dockerfile
+++ Dockerfile
@@ -17,7 +17,9 @@ RUN \
     'mkdocs-minify-plugin>=0.2' \
     'mkdocs-git-revision-date-localized-plugin>=0.4'
 WORKDIR /
-COPY entrypoint.sh entrypoint.sh
+COPY docker-entrypoint.sh /usr/local/bin/entrypoint.sh
+RUN ln -s /usr/local/bin/docker-entrypoint.sh /
+    chmod +x /entrypoint.sh
 EXPOSE 8000
 ENTRYPOINT ["entrypoint.sh"]
 CMD ["serve", "--dev-addr=0.0.0.0:8000"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,15 @@
-#!/bin/bash
-set -x
-while $1
-do
+#!/bin/sh
   INPUT_STRING=$1
   case $INPUT_STRING in
-	produce)
-		tar -czvf produced.tar.gz docs/
-		;;
-	serve)
-        tar -xvzf produced.tar.gz
-		mkdocs $1
-		break
-		;;
-	*)
-		echo "Sorry, I don't understand"
-		;;
+        produce)
+                tar -czvf produced.tar.gz docs/ sites/ mkdocs.yml
+                ;;
+        serve)
+        tar -c / -xvzf produced.tar.gzs
+                mkdocs $1
+                break
+                ;;
+        *)
+                echo "Sorry, I don't understand"
+                ;;
   esac
-dones
